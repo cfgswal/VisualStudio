c636aab36a367d256af719bc99412856dba3fe78
cfgswal <Ramon@MacBokk-Pro-de-Ramon.local>
2016-01-13 20:01:47 +0100

    Medallero

---
+++ Medallero/Medallero/Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ALEMANIA = New System.Windows.Forms.Button()
+        Me.ESPAÑA = New System.Windows.Forms.Button()
+        Me.USA = New System.Windows.Forms.Button()
+        Me.ITALIA = New System.Windows.Forms.Button()
+        Me.CAMPEON = New System.Windows.Forms.Button()
+        Me.SALIR = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ALEMANIA
+        '
+        Me.ALEMANIA.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ALEMANIA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ALEMANIA.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ALEMANIA.Location = New System.Drawing.Point(193, 262)
+        Me.ALEMANIA.Name = "ALEMANIA"
+        Me.ALEMANIA.Size = New System.Drawing.Size(351, 98)
+        Me.ALEMANIA.TabIndex = 0
+        Me.ALEMANIA.Text = "ALEMANIA"
+        Me.ALEMANIA.UseVisualStyleBackColor = False
+        '
+        'ESPAÑA
+        '
+        Me.ESPAÑA.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ESPAÑA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ESPAÑA.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ESPAÑA.Location = New System.Drawing.Point(869, 262)
+        Me.ESPAÑA.Name = "ESPAÑA"
+        Me.ESPAÑA.Size = New System.Drawing.Size(351, 98)
+        Me.ESPAÑA.TabIndex = 1
+        Me.ESPAÑA.Text = "ESPAÑA"
+        Me.ESPAÑA.UseVisualStyleBackColor = False
+        '
+        'USA
+        '
+        Me.USA.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.USA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.USA.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.USA.Location = New System.Drawing.Point(193, 479)
+        Me.USA.Name = "USA"
+        Me.USA.Size = New System.Drawing.Size(351, 98)
+        Me.USA.TabIndex = 2
+        Me.USA.Text = "USA"
+        Me.USA.UseVisualStyleBackColor = False
+        '
+        'ITALIA
+        '
+        Me.ITALIA.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ITALIA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ITALIA.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ITALIA.Location = New System.Drawing.Point(869, 470)
+        Me.ITALIA.Name = "ITALIA"
+        Me.ITALIA.Size = New System.Drawing.Size(351, 98)
+        Me.ITALIA.TabIndex = 3
+        Me.ITALIA.Text = "ITALIA"
+        Me.ITALIA.UseVisualStyleBackColor = False
+        '
+        'CAMPEON
+        '
+        Me.CAMPEON.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.CAMPEON.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.CAMPEON.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.CAMPEON.Location = New System.Drawing.Point(531, 711)
+        Me.CAMPEON.Name = "CAMPEON"
+        Me.CAMPEON.Size = New System.Drawing.Size(351, 105)
+        Me.CAMPEON.TabIndex = 4
+        Me.CAMPEON.Text = "CAMPEON"
+        Me.CAMPEON.UseVisualStyleBackColor = False
+        '
+        'SALIR
+        '
+        Me.SALIR.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.SALIR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.SALIR.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.SALIR.Location = New System.Drawing.Point(1109, 913)
+        Me.SALIR.Name = "SALIR"
+        Me.SALIR.Size = New System.Drawing.Size(351, 107)
+        Me.SALIR.TabIndex = 5
+        Me.SALIR.Text = "SALIR"
+        Me.SALIR.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(597, 262)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 98)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(1326, 262)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 98)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(597, 479)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 98)
+        Me.TextBox3.TabIndex = 8
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(1326, 470)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 98)
+        Me.TextBox4.TabIndex = 9
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox5.Location = New System.Drawing.Point(531, 913)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(351, 98)
+        Me.TextBox5.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(191, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1209, 91)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "MEDALLERO DE SYDNEY 2000"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1532, 1063)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.SALIR)
+        Me.Controls.Add(Me.CAMPEON)
+        Me.Controls.Add(Me.ITALIA)
+        Me.Controls.Add(Me.USA)
+        Me.Controls.Add(Me.ESPAÑA)
+        Me.Controls.Add(Me.ALEMANIA)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ALEMANIA As Button
+    Friend WithEvents ESPAÑA As Button
+    Friend WithEvents USA As Button
+    Friend WithEvents ITALIA As Button
+    Friend WithEvents CAMPEON As Button
+    Friend WithEvents SALIR As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Medallero/Medallero/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Dim cont = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CAMPEON.Enabled = False
+        SALIR.Enabled = False
+        TextBox5.ReadOnly = True
+        TextBox1.ReadOnly = True
+        TextBox2.ReadOnly = True
+        TextBox3.ReadOnly = True
+        TextBox4.ReadOnly = True
+        TextBox5.Enabled = False
+    End Sub
+    Sub contador()
+        cont = cont + 1
+        If cont = 4 Then
+            CAMPEON.Enabled = True
+
+        End If
+    End Sub
+
+
+    Private Sub CAMPEON_Click(sender As Object, e As EventArgs) Handles CAMPEON.Click
+        TextBox5.Text = "USA"
+        SALIR.Enabled = True
+        TextBox5.Enabled = True
+
+    End Sub
+
+    Private Sub ALEMANIA_Click(sender As Object, e As EventArgs) Handles ALEMANIA.Click
+        TextBox1.Text = 20
+        contador()
+        RemoveHandler ALEMANIA.Click, AddressOf ALEMANIA_Click
+    End Sub
+
+    Private Sub ESPAÑA_Click(sender As Object, e As EventArgs) Handles ESPAÑA.Click
+        TextBox2.Text = 25
+        contador()
+        RemoveHandler ESPAÑA.Click, AddressOf ESPAÑA_Click
+    End Sub
+
+    Private Sub USA_Click(sender As Object, e As EventArgs) Handles USA.Click
+        TextBox3.Text = 45
+        contador()
+        RemoveHandler USA.Click, AddressOf USA_Click
+    End Sub
+
+    Private Sub ITALIA_Click(sender As Object, e As EventArgs) Handles ITALIA.Click
+        TextBox4.Text = 12
+        contador()
+        RemoveHandler ITALIA.Click, AddressOf ITALIA_Click
+    End Sub
+
+    Private Sub SALIR_Click(sender As Object, e As EventArgs) Handles SALIR.Click
+        Environment.Exit(0)
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.MouseEnter
+
+        CAMPEON.Enabled = False
+        SALIR.Enabled = False
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        AddHandler ALEMANIA.Click, AddressOf ALEMANIA_Click
+        AddHandler ESPAÑA.Click, AddressOf ESPAÑA_Click
+        AddHandler ITALIA.Click, AddressOf ITALIA_Click
+        AddHandler USA.Click, AddressOf USA_Click
+        cont = 0
+    End Sub
+
+
+End Class
